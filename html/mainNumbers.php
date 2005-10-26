b1216f35cf657a872daed37ac8f5093f2f561dc3
esslinga <esslinga@408bbc16-3416-0410-a318-dc4756dda0eb>
2005-10-26 14:44:09 +0000

    Removed nonexistent department from home page; added addresses to the main numbers page

--- html/mainNumbers.php
+++ html/mainNumbers.php
@@ -9,60 +9,95 @@
 	
 	<table>
 	<tr><th>Animal Shelter:</th>
-		<td>349-3492</td></tr>
+		<td>349-3492</td>
+		<td>3410 Old S.R. 37 South</td></tr>
+	<tr><th>Blucher Poole Wastewater Plant:</th>
+		<td>876-4875</td>
+		<td>5555 N. Bottom Rd</td></tr>
 	<tr><th>City Clerk:</th>
-		<td>349-3408</td></tr>
+		<td>349-3408</td>
+		<td>401 N. Morton St, Suite 110</td></tr>
 	<tr><th>City Hall:</th>
-		<td>349-3400</td></tr>
+		<td>349-3400</td>
+		<td>401 N. Morton St</td></tr>
 	<tr><th>Controller:</th>
-		<td>349-3412</td></tr>
+		<td>349-3412</td>
+		<td>401 N. Morton St, Suite 240</td></tr>
 	<tr><th>Community &amp; Family Resources:</th>
-		<td>349-3430</td></tr>
+		<td>349-3430</td>
+		<td>401 N. Morton St, Suite 260</td></tr>
 	<tr><th>Council Office:</th>
-		<td>349-3409</td></tr>
+		<td>349-3409</td>
+		<td>401 N. Morton St, Suite 110</td></tr>
+	<tr><th>Dillman Wastewater Plant:</th>
+		<td>824-4900</td>
+		<td>100 W. Dillman Rd</td></tr>
 	<tr><th>Employee Services:</th>
-		<td>349-3404</td></tr>
+		<td>349-3404</td>
+		<td>401 N. Morton St, Suite 230</td></tr>
 	<tr><th>Engineering:</th>
-		<td>349-3417</td></tr>
+		<td>349-3417</td>
+		<td>401 N. Morton St, Suite 130</td></tr>
 	<tr><th>Fire Department:</th>
-		<td>332-9763</td></tr>
+		<td>332-9763</td>
+		<td>300 E. Fourth St</td></tr>
 	<tr><th>Fleet Maintenance:</th>
-		<td>349-3494</td></tr>
+		<td>349-3494</td>
+		<td>1969 S. Henderson</td></tr>
 	<tr><th>H.A.N.D.:</th>
-		<td>349-3401 (Housing)
-			349-3420 (Neighborhood)</td></tr>
+		<td>349-3401 (Housing)<br />349-3420 (Neighborhood)</td>
+		<td>401 N. Morton St, Suite 130</td></tr>
 	<tr><th>Housing Authority:</th>
-		<td>339-3491</td></tr>
+		<td>339-3491</td>
+		<td>1007 N. Summitt</td></tr>
 	<tr><th>Human Rights Commission:</th>
-		<td>349-3429</td></tr>
+		<td>349-3429</td>
+		<td>401 N. Morton St, Suite 220</td></tr>
 	<tr><th>Information &amp; Technology Services:</th>
-		<td>349-3454</td></tr>
+		<td>349-3454</td>
+		<td>401 N. Morton St, Suite 150</td></tr>
 	<tr><th>Legal Department:</th>
-		<td>349-3426</td></tr>
+		<td>349-3426</td>
+		<td>401 N. Morton St, Suite 220</td></tr>
 	<tr><th>Mayor's Office:</th>
-		<td>349-3406</td></tr>
+		<td>349-3406</td>
+		<td>401 N. Morton St, Suite 210</td></tr>
+	<tr><th>Monroe Water Plant:</th>
+		<td>824-9311</td>
+		<td>7470 S. Shields Ridge Rd</td></tr>
 	<tr><th>Parking Enforcement:</th>
-		<td>349-3436</td></tr>
+		<td>349-3436</td>
+		<td>401 N. Morton St, Suite 120</td></tr>
 	<tr><th>Parks &amp; Recreation:</th>
-		<td>349-3700</td></tr>
+		<td>349-3700</td>
+		<td>401 N. Morton St, Suite 250</td></tr>
 	<tr><th>Planning Department:</th>
-		<td>349-3423</td></tr>
+		<td>349-3423</td>
+		<td>401 N. Morton St, Suite 160</td></tr>
 	<tr><th>Police:</th>
-		<td>349-3312</td></tr>
+		<td>349-3312</td>
+		<td>220 E. Third St</td></tr>
 	<tr><th>Public Works:</th>
-		<td>349-3410</td></tr>
+		<td>349-3410</td>
+		<td>401 N. Morton St, Suite 130</td></tr>
 	<tr><th>Risk Management:</th>
-		<td>349-3438</td></tr>
+		<td>349-3438</td>
+		<td>401 N. Morton St, Suite 210</td></tr>
 	<tr><th>Sanitation Department:</th>
-		<td>349-3496</td></tr>
+		<td>349-3496</td>
+		<td>3406 Old S.R. 37 South</td></tr>
 	<tr><th>Street Department:</th>
-		<td>349-3448
-			349-3449
-			349-3452 (Traffic)</td></tr>
+		<td>349-3448</td>
+		<td>1981 S. Henderson</td></tr>
+	<tr><th>Traffic</th>
+		<td>349-3452</td>
+		<td>121 W. Allen</td></tr>
 	<tr><th>Transit:</th>
-		<td>332-5688</td></tr>
+		<td>332-5688</td>
+		<td>130 W. Grimes Ln</td></tr>
 	<tr><th>Utilities:</th>
-		<td>339-1444</td></tr>
+		<td>339-1444</td>
+		<td>1969 S. Henderson</td></tr>
 	</table>
 
 </div>
