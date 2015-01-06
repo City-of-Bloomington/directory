51fd1a4c9632c4828b35848cc393030ecf115cd1
inghamn <inghamn@bloomington.in.gov>
2015-01-06 10:02:14 -0500

    Added build script for SCSS

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-BUILD=./build
-DIST=./dist
+DIR=`pwd`
+BUILD=$DIR/build
+DIST=$DIR/dist
 
 if [ ! -d $BUILD ]
 	then mkdir $BUILD
@@ -10,6 +11,14 @@ if [ ! -d $DIST ]
 	then mkdir $DIST
 fi
 
-rsync -rlv --exclude-from=./buildignore --delete ./ ./build/
+# The PHP code does not need to actually build anything.
+# Just copy all the files into the build
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
 
+# Compile the SCSS
+cd $DIR/public/css
+./build_css.sh
+cd $DIR
+
+# Create a distribution tarball of the build
 tar czvf $DIST/directory.tar.gz --transform=s/build/directory/ $BUILD
