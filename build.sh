9b3aeafed445d5ea5fb91c22fef19b63dad6c1bd
inghamn <inghamn@bloomington.in.gov>
2015-01-30 12:04:32 -0500

    Created a build script for the Language files

--- build.sh
+++ build.sh
@@ -11,14 +11,19 @@ if [ ! -d $DIST ]
 	then mkdir $DIST
 fi
 
-# The PHP code does not need to actually build anything.
-# Just copy all the files into the build
-rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
-
 # Compile the SCSS
 cd $DIR/public/css
 ./build_css.sh
 cd $DIR
+
+# Compile the Lanague files
+cd $DIR/language
+./build_lang.sh
+cd $DIR
+
+# The PHP code does not need to actually build anything.
+# Just copy all the files into the build
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
 
 # Create a distribution tarball of the build
 tar czvf $DIST/directory.tar.gz --transform=s/build/directory/ $BUILD
