2746a7e7f50eddd574ff2f9a5d6c21809c0c362f
inghamn <inghamn@bloomington.in.gov>
2016-02-23 10:42:18 -0500

    Improved build script

--- build.sh
+++ build.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
+APPNAME=directory
 DIR=`pwd`
 BUILD=$DIR/build
-DIST=$DIR/dist
 
 if [ ! -d $BUILD ]
 	then mkdir $BUILD
 fi
-
-if [ ! -d $DIST ]
-	then mkdir $DIST
-fi
-
-# Compile the SCSS
-cd $DIR/public/css
-./build_css.sh
-cd $DIR
 
 # Compile the Lanague files
 cd $DIR/language
 ./build_lang.sh
 cd $DIR
 
+# Compile the SASS
+cd $DIR/public/css
+./build_css.sh
+cd $DIR
+
 # The PHP code does not need to actually build anything.
 # Just copy all the files into the build
-rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/
-
-# Create a distribution tarball of the build
-tar czvf $DIST/directory.tar.gz --transform=s/build/directory/ $BUILD
+rsync -rlv --exclude-from=$DIR/buildignore --delete $DIR/ $BUILD/$APPNAME
+cd $BUILD
+tar czvf $APPNAME.tar.gz $APPNAME
