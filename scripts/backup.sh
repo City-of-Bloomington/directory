e5c52298410b7e1cbdfae7d45ec0d43d39fd2320
inghamn <inghamn@bloomington.in.gov>
2018-05-17 10:18:42 -0400

    Backup script now backs up the entire SITE_HOME
    Fixes #546

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,40 +4,42 @@
 # @copyright Copyright 2011-2018 City of Bloomington, Indiana
 # @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
 APPLICATION_NAME="directory"
-MYSQLDUMP="/usr/bin/mysqldump"
 MYSQL_CREDENTIALS="/etc/cron.daily/backup.d/${APPLICATION_NAME}.cnf"
 BACKUP_DIR="/srv/backups/${APPLICATION_NAME}"
 APPLICATION_HOME="/srv/sites/${APPLICATION_NAME}"
+SITE_HOME="/srv/data/${APPLICATION_NAME}"
 
 MYSQL_DBNAME="${APPLICATION_NAME}"
 
+# Update the timestamp for zabbix monitoring
+touch "/var/log/cron/${APPLICATION_NAME}"
+
+#----------------------------------------------------------
+# Backup
+# Creates a tarball containing a full snapshot of the data in the site
+#----------------------------------------------------------
 # How many days worth of tarballs to keep around
 num_days_to_keep=5
 
-#----------------------------------------------------------
-# No Editing Required below this line
-#----------------------------------------------------------
 now=`date +%s`
 today=`date +%F`
 
 if [ ! -d "${BACKUP_DIR}" ]
 	then mkdir "${BACKUP_DIR}"
 fi
-cd "${BACKUP_DIR}"
-mkdir $today
 
 # Dump the database
-$MYSQLDUMP --defaults-extra-file="${MYSQL_CREDENTIALS}" $MYSQL_DBNAME > $today/$MYSQL_DBNAME.sql
+cd "${SITE_HOME}"
+mysqldump --defaults-extra-file="${MYSQL_CREDENTIALS}" $MYSQL_DBNAME > $MYSQL_DBNAME.sql
 
-# Copy any data directories into this directory, so they're backed up, too.
-# For example, if we had a media directory....
-#cp -R $APPLICATION_HOME/data/media $today/media
-
-# Tarball the Data
-tar czf $today.tar.gz $today
-rm -Rf $today
+# Tarball all the data
+cd ..
+data=`basename "${SITE_HOME}"`
+tar czf $today.tar.gz "${data}"
+mv $today.tar.gz "${BACKUP_DIR}"
 
 # Purge any backup tarballs that are too old
+cd "${BACKUP_DIR}"
 for file in `ls`
 do
 	atime=`stat -c %Y $file`
