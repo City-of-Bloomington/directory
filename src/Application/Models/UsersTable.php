335d594b31eba27e5477c422cbc85d42e28b5455
inghamn <inghamn@bloomington.in.gov>
2018-12-03 10:09:14 -0500

    Don't use count() on null
    Updates #27

--- src/Application/Models/UsersTable.php
+++ src/Application/Models/UsersTable.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * @copyright 2014 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
- * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @copyright 2014-2018 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
 namespace Application\Models;
 
@@ -22,7 +21,7 @@ class UsersTable extends TableGateway
 	public function find($fields=null, $order='lastname', $paginated=false, $limit=null)
 	{
 		$select = new Select('users');
-		if (count($fields)) {
+		if ($fields) {
 			foreach ($fields as $key=>$value) {
                 $select->where([$key=>$value]);
 			}
