c1d5379c03a5bfda879093a871c50bc844ceaf03
Cliff Ingham <inghamn@bloomington.in.gov>
2024-10-25 13:48:23 -0400

    Updated the exportPhotos script
    It has been many years since we've used this script.  It had not been updated to work with the last rewrite of the system.
    
    Updates #52

--- scripts/extractPhotos.php
+++ scripts/extractPhotos.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * @copyright 2014 City of Bloomington, Indiana
- * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
- * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @copyright 2024 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE
  */
+declare (strict_types=1);
 include '../bootstrap.php';
 
 use Application\Models\DepartmentGateway;
 
-$gateway = new DepartmentGateway();
+$dn    = 'OU=Utilities,OU=Departments,DC=cob,DC=bloomington,DC=in,DC=gov';
+$staff = DepartmentGateway::search($dn, []);
+foreach ($staff as $p) {
+    $user = $p->username;
+    $data = $p->getPhoto();
+    echo "$user\n";
 
-$people = $gateway->getPeople();
-foreach ($people as $p) {
-    $username = $p->username;
-    echo "$username\n";
-    $data = $gateway->getPhoto($username);
     if ($data) {
-        file_put_contents("./users/$username.jpg", $data);
+        file_put_contents("./users/$user.jpg", $data);
     }
 }
